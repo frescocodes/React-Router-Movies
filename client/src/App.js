cf0a895de13ebe11ed7769901b6dad0237ee01b1
fresco <fresco@pop-os.localdomain>
2019-09-17 16:33:55 -0400

    setup, wrapped app in router, set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-
+import { Route, Link } from "react-router-dom";
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList'
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -13,7 +14,11 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+
+      <Route exact path='/' component={MovieList}/>
+      <Route path='/movies/:id' component={Movie}/>
     </div>
+    
   );
 };
 
