20e195c179fd116dfdac6a34d87c8e4bf82485ea
fresco <fresco@pop-os.localdomain>
2019-09-18 12:13:20 -0400

    MVP done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Route, Link } from "react-router-dom";
 import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList'
+import MovieList from './Movies/MovieList';
+import Movie from "./Movies/Movie";
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -13,10 +14,12 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
 
       <Route exact path='/' component={MovieList}/>
-      <Route path='/movies/:id' component={Movie}/>
+      <Route path='/movies/:id' render={props => 
+      (<Movie 
+        {...props}
+      />)}/>
     </div>
     
   );
